8b6e7eed47a06841b960507255ef9414100efa8e
kebapcore <samilnurdoganforsocial@gmail.com>
2026-02-20 14:29:24 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
-    root: path.resolve(__dirname, 'public'),
+    root: path.resolve(__dirname, '.'),
     server: {
         port: 5173,
         strictPort: false,
